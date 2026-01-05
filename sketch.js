519e6d0839f153cb0deca83c06fecd8d2b4e2df7
Bryanna Huang <bryannahuang2016@gmail.com>
2026-01-04 22:03:42 -0500

    color imp works

--- sketch.js
+++ sketch.js
@@ -1,26 +1,33 @@
 // setup() is called once at page-load
 function setup() {
-    createCanvas(800,600); // make an HTML canvas element width x height pixels
+    createCanvas(300,500); // make an HTML canvas element width x height pixels
     colorMode(RGB, 100);
 }
 
 // draw() is called 60 times per second
 function draw() {
-    background(255);
+    background(90);
   
     let hr = hour();
     let min = minute();
     let sec = second();
 
-    fill(0,0, 100-80/24*hr);
-    ellipse(75, 400 - hr*200/24, 50, hr*400/24);
+    noStroke(); 
   
-    fill(100-80/60*min, 0, 0);
-    ellipse(150, 400 - min*200/60, 50, min*400/60);
+    let h = map(hr, 0, 12, 20, 80);
+    let m = map(min, 0, 59, 20, 80);
+    let s = map(sec, 0, 59, 20, 80);
+
+    fill(0,0, 100-h);
+    noStroke();
+    ellipse(75, 450 - hr*200/24, 50, 10+hr*400/24);
   
-    fill(0,100-80/60*sec, 0);
-    ellipse(225, 400 - sec*200/60, 50, sec*400/60);
+    fill(100-m, 0, 0);
+    noStroke();
+    ellipse(150, 450 - min*200/60, 50, 10+min*400/60);
   
-  
+    fill(0,100-s, 0);
+    noStroke();
+    ellipse(225, 450 - sec*200/60, 50, 10+sec*400/60);
 
 }
