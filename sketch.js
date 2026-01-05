67086967c97a4b42f23e5ccb918bf113e32dad8a
Bryanna Huang <bryannahuang2016@gmail.com>
2026-01-04 22:10:17 -0500

    centered on pg

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // setup() is called once at page-load
 function setup() {
-    createCanvas(300,500); // make an HTML canvas element width x height pixels
+    createCanvas(600,600); // make an HTML canvas element width x height pixels
     colorMode(RGB, 100);
 }
 
@@ -11,23 +11,29 @@ function draw() {
     let hr = hour();
     let min = minute();
     let sec = second();
+  
+    fill(0,0,0);
+    circle(200, 275, 10);
+    fill(0,0,0);
+    circle(200, 325, 10);
 
     noStroke(); 
-  
+
     let h = map(hr, 0, 12, 20, 80);
     let m = map(min, 0, 59, 20, 80);
     let s = map(sec, 0, 59, 20, 80);
 
     fill(0,0, 100-h);
     noStroke();
-    ellipse(75, 450 - hr*200/24, 50, 10+hr*400/24);
+    ellipse(85, 300, 50+hr*2.5, 10+hr*400/24);
+  
   
     fill(100-m, 0, 0);
     noStroke();
-    ellipse(150, 450 - min*200/60, 50, 10+min*400/60);
+    ellipse(315, 300, 50+min*2, 10+min*400/60);
   
     fill(0,100-s, 0);
     noStroke();
-    ellipse(225, 450 - sec*200/60, 50, 10+sec*400/60);
+    ellipse(500, 300, 50+sec*2, 10+sec*400/60);
 
 }
