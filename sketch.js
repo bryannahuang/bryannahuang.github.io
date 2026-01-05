0288bc250571550a71a7d452a7c357f06a5315fe
Bryanna Huang <bryannahuang2016@gmail.com>
2026-01-04 20:02:38 -0500

    version1

--- sketch.js
+++ sketch.js
@@ -1,20 +1,26 @@
 // setup() is called once at page-load
 function setup() {
     createCanvas(800,600); // make an HTML canvas element width x height pixels
+    colorMode(RGB, 100);
 }
 
 // draw() is called 60 times per second
 function draw() {
+    background(255);
+  
     let hr = hour();
     let min = minute();
     let sec = second();
 
-    background(225);
-    textSize(32);
-    fill(180);
-    text(hr, 10, 30);
-    fill(100);
-    text(min, 10, 60);
-    fill(0);
-    text(sec, 10, 90);
+    fill(0,0, 100-80/24*hr);
+    ellipse(75, 400 - hr*200/24, 50, hr*400/24);
+  
+    fill(100-80/60*min, 0, 0);
+    ellipse(150, 400 - min*200/60, 50, min*400/60);
+  
+    fill(0,100-80/60*sec, 0);
+    ellipse(225, 400 - sec*200/60, 50, sec*400/60);
+  
+  
+
 }
